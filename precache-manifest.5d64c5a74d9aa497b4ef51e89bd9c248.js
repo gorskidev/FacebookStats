9e3d2b39c03f91f17adee3a2391770a60ba25173
Maciek <maciej.gorski995@gmail.com>
2020-03-20 13:12:26 +0100

    Updates

---
+++ precache-manifest.5d64c5a74d9aa497b4ef51e89bd9c248.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f684f2b2ab29a9034b10a94875a82fef",
+    "url": "/FacebookStats/index.html"
+  },
+  {
+    "revision": "e0c3ee3ec0ef5dd3cf47",
+    "url": "/FacebookStats/static/css/main.08ff50f2.chunk.css"
+  },
+  {
+    "revision": "681a58211aaef0e9ca80",
+    "url": "/FacebookStats/static/js/2.1030dfea.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/FacebookStats/static/js/2.1030dfea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c3ee3ec0ef5dd3cf47",
+    "url": "/FacebookStats/static/js/main.28df1041.chunk.js"
+  },
+  {
+    "revision": "1800d376a09d09682514",
+    "url": "/FacebookStats/static/js/runtime-main.1b7f2c9a.js"
+  }
+]);
